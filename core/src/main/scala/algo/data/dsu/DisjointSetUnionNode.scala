0c7f6cc529f33e6a76f5318025d2330c2a55dde5
Taichi Yamakawa <xirckey@gmail.com>
2021-05-25 23:02:32 +0900

    Commonize DisjointSetUnion's node union

---
+++ core/src/main/scala/algo/data/dsu/DisjointSetUnionNode.scala
@@ -0,0 +1,34 @@
+package algo.data.dsu
+
+import cats.kernel.CommutativeSemigroup
+import cats.syntax.semigroup._
+
+private final case class DisjointSetUnionNode[V](
+    leader: Int,
+    rank: Int,
+    value: V
+)
+
+private object DisjointSetUnionNode {
+
+  def unite[V: CommutativeSemigroup](
+      leader: DisjointSetUnionNode[V],
+      follower: DisjointSetUnionNode[V]
+  ): (DisjointSetUnionNode[V], DisjointSetUnionNode[V]) = {
+    assert(leader.rank >= follower.rank)
+    val newFollower = follower.copy(leader = leader.leader)
+    val newLeader = {
+      val newValue =
+        leader.value |+| follower.value
+      val newRank =
+        if (leader.rank == follower.rank) {
+          leader.rank + 1
+        } else {
+          leader.rank
+        }
+      leader.copy(rank = newRank, value = newValue)
+    }
+    (newLeader, newFollower)
+  }
+
+}
--- core/src/main/scala/algo/data/dsu/mutable/DefaultDisjointSetUnion.scala
+++ core/src/main/scala/algo/data/dsu/mutable/DefaultDisjointSetUnion.scala
@@ -1,29 +1,18 @@
 package algo.data.dsu.mutable
 
+import algo.data.dsu.DisjointSetUnionNode
 import cats.kernel.CommutativeSemigroup
-import cats.syntax.semigroup._
 
 import scala.collection.mutable.ArrayBuffer
-
-private object DefaultDisjointSetUnion {
-
-  final case class Node[V: CommutativeSemigroup](
-      leader: Int,
-      rank: Int,
-      value: V
-  )
-
-}
 
 private final class DefaultDisjointSetUnion[V: CommutativeSemigroup](
     values: IterableOnce[V]
 ) extends DisjointSetUnion[V] {
-  import DefaultDisjointSetUnion._
 
-  private val nodes: ArrayBuffer[Node[V]] = {
+  private val nodes: ArrayBuffer[DisjointSetUnionNode[V]] = {
     values.iterator.zipWithIndex
       .map { case (value, index) =>
-        Node(index, 0, value)
+        DisjointSetUnionNode(index, 0, value)
       }
       .to(ArrayBuffer)
   }
@@ -42,32 +31,19 @@ private final class DefaultDisjointSetUnion[V: CommutativeSemigroup](
     val lu = findNode(u).leader
     val lv = findNode(v).leader
     if (lu == lv) {
-      return this
+      this
+    } else {
+      val (leader, follower) =
+        if (nodes(lu).rank > nodes(lv).rank) (lu, lv) else (lv, lu)
+      val (newLeader, newFollower) =
+        DisjointSetUnionNode.unite(nodes(leader), nodes(follower))
+      nodes(follower) = newFollower
+      nodes(leader) = newLeader
+      this
     }
-
-    val (leader, follower) =
-      if (nodes(lu).rank > nodes(lv).rank) (lu, lv) else (lv, lu)
-
-    val newFollower = nodes(follower).copy(leader = leader)
-    nodes(follower) = newFollower
-
-    val newLeadersValue =
-      nodes(leader).value |+| nodes(follower).value
-    val newLeadersRank = {
-      if (nodes(leader).rank == nodes(follower).rank) {
-        nodes(leader).rank + 1
-      } else {
-        nodes(leader).rank
-      }
-    }
-    val newLeader =
-      nodes(leader).copy(rank = newLeadersRank, value = newLeadersValue)
-    nodes(leader) = newLeader
-
-    this
   }
 
-  private def findNode(v: Int): Node[V] = {
+  private def findNode(v: Int): DisjointSetUnionNode[V] = {
     if (v < 0 || v >= size)
       throw new IndexOutOfBoundsException(s"Index out of range [0, $size): $v")
     if (v != nodes(v).leader) {
