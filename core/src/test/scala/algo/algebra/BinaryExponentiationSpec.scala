ca94b2b8d2ed63874bb6d5b93efb257680582976
Taichi Yamakawa <xirckey@gmail.com>
2021-09-26 21:27:30 +0900

    Refactor BinaryExponentiationSpec
    Use plain assert instead.

--- core/src/test/scala/algo/algebra/BinaryExponentiationSpec.scala
+++ core/src/test/scala/algo/algebra/BinaryExponentiationSpec.scala
@@ -8,36 +8,36 @@ final class BinaryExponentiationSpec extends BaseSpec {
 
   "use the default instance of BinaryExponentiation[Int]" in {
 
-    (2: Int).binexp(10) shouldBe 1024
-    (10: Int).binexp(9) shouldBe 1_000_000_000
+    assert((2: Int).binexp(10) === 1024)
+    assert((10: Int).binexp(9) === 1_000_000_000)
 
   }
 
   "use the default instance of BinaryExponentiation[Long]" in {
 
-    (2L: Long).binexp(10) shouldBe 1024
-    (10L: Long).binexp(9) shouldBe 1_000_000_000
+    assert((2L: Long).binexp(10) === 1024L)
+    assert((10L: Long).binexp(9) === 1_000_000_000L)
 
   }
 
   "use the default instance of BinaryExponentiation[Float]" in {
 
-    (2f: Float).binexp(10) shouldBe 1024f
-    (10f: Float).binexp(9) shouldBe 1e9f
+    assert((2f: Float).binexp(10) === 1024f)
+    assert((10f: Float).binexp(9) === 1e9f)
 
   }
 
   "use the default instance of BinaryExponentiation[Double]" in {
 
-    (2.0: Double).binexp(10) shouldBe 1024.0
-    (10.0: Double).binexp(9) shouldBe 1e9
+    assert((2.0: Double).binexp(10) === 1024.0)
+    assert((10.0: Double).binexp(9) === 1e9)
 
   }
 
   "use the default instance of BinaryExponentiation[BigInt]" in {
 
-    BigInt(2).binexp(10) shouldBe BigInt(1024)
-    BigInt(10).binexp(100) shouldBe BigInt(10).pow(100)
+    assert(BigInt(2).binexp(10) === BigInt(1024))
+    assert(BigInt(10).binexp(100) === BigInt(10).pow(100))
 
   }
 
@@ -45,7 +45,7 @@ final class BinaryExponentiationSpec extends BaseSpec {
 
     implicit val plus: BinaryExponentiation[Int] =
       BinaryExponentiation.instance(0, _ + _)
-    100.binexp(100) shouldBe 10_000
+    assert(100.binexp(100) === 10_000)
 
   }
 
@@ -53,7 +53,7 @@ final class BinaryExponentiationSpec extends BaseSpec {
 
     implicit val plus: BinaryExponentiation[Int] =
       BinaryExponentiation.fromMonoid
-    100.binexp(100) shouldBe 10_000
+    assert(100.binexp(100) === 10_000)
 
   }
 
@@ -61,7 +61,7 @@ final class BinaryExponentiationSpec extends BaseSpec {
 
     implicit val plus: BinaryExponentiation[Int] =
       Monoid[Int].toBinaryExponentiation
-    100.binexp(100) shouldBe 10_000
+    assert(100.binexp(100) === 10_000)
 
   }
 
@@ -76,10 +76,7 @@ final class BinaryExponentiationSpec extends BaseSpec {
     val expected =
       Iterator.fill(1_000_000)(BigInt(3)).fold(BigInt(1))(mulModFunc)
 
-    // O(log(N))
-    val actual = BigInt(3).binexp(1_000_000)
-
-    actual shouldBe expected
+    assert(BigInt(3).binexp(1_000_000) === expected)
 
   }
 
