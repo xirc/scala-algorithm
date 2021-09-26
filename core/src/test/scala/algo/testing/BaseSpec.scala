53dfa7d531e5036c55129452a75c37ba37358c1b
Taichi Yamakawa <xirckey@gmail.com>
2021-09-26 21:26:27 +0900

    Add TypeCheckedTripleEquals to BaseSpec

--- core/src/test/scala/algo/testing/BaseSpec.scala
+++ core/src/test/scala/algo/testing/BaseSpec.scala
@@ -1,7 +1,12 @@
 package algo.testing
 
+import org.scalactic.TypeCheckedTripleEquals
 import org.scalatest.Inside
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 
-abstract class BaseSpec extends AnyWordSpecLike with Matchers with Inside
+abstract class BaseSpec
+    extends AnyWordSpecLike
+    with TypeCheckedTripleEquals
+    with Matchers
+    with Inside
