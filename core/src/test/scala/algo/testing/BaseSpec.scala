b17b62e8c504658ec1f930d2ffcc86923379d16b
Taichi Yamakawa <xirckey@gmail.com>
2021-09-28 22:45:41 +0900

    Remove should matchers from BaseSpec

--- core/src/test/scala/algo/testing/BaseSpec.scala
+++ core/src/test/scala/algo/testing/BaseSpec.scala
@@ -2,11 +2,9 @@ package algo.testing
 
 import org.scalactic.TypeCheckedTripleEquals
 import org.scalatest.Inside
-import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 
 abstract class BaseSpec
     extends AnyWordSpecLike
     with TypeCheckedTripleEquals
-    with Matchers
     with Inside
